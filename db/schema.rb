80800edea19fd43fa1168b9cd7a324b97ea8ffde
Paco Luna <pakoo_15@hotmail.com>
2019-10-20 12:30:43 -0500

    Add hour to the entries table
    Change entries controller, views and database table to allow an
    hour field.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_19_180146) do
+ActiveRecord::Schema.define(version: 2019_10_20_164632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_10_19_180146) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.time "hour"
   end
 
   create_table "users", force: :cascade do |t|
