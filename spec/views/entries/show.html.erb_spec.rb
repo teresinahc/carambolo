51940f7c7d3127b3eaeb494850cba65de632e0b1
Eduardo <jeduardo824@gmail.com>
2019-12-08 15:05:42 -0300

    change views to work with enumerized feelings and I18n

--- spec/views/entries/show.html.erb_spec.rb
+++ spec/views/entries/show.html.erb_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "entries/show", type: :view do
-  let(:entry) { create(:entry, feeling: 'Feeling', description: 'MyText') }
+  let(:entry) { create(:entry, feeling: :sad, description: 'MyText') }
   before(:each) do
     @entry = assign(:entry, entry)
   end
 
   it "renders attributes in <p>" do
     render
-    expect(rendered).to match(/Feeling/)
+    expect(rendered).to match(/Sad/)
     expect(rendered).to match(/MyText/)
   end
 end
