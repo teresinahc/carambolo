e9d68f43319a8c98f463958cbe7527f88d0ce38b
Renata Marques <Renata>
2019-12-15 22:48:10 -0300

    deleting comments from specs

--- spec/controllers/entries_controller_spec.rb
+++ spec/controllers/entries_controller_spec.rb
@@ -1,7 +1,6 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe EntriesController, type: :controller do
-
   let(:user) { create(:user) }
   let(:valid_attributes) {
     attributes_for(:entry, user_id: user.id)
