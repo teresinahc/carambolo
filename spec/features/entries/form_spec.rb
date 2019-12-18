0ddbe51071c2d5ac0807af41fbe820f2cab409b8
Renata Marques <renata.marques@codeminer42.com>
2019-12-18 17:59:30 -0300

    wip feature spec for form

--- spec/features/entries/form_spec.rb
+++ spec/features/entries/form_spec.rb
@@ -2,12 +2,32 @@ require "rails_helper"
 
 RSpec.describe 'form', type: :feature do
   describe 'submit a form' do
-    it "redirect to" do
-      # expect(subject).to redirect_to("/entries/:id(.:format)")
+    let(:user) { create(:user) }
+
+    before do
+      sign_in user
+      visit new_entry_path
+
+      fill_in "Feeling", with: 'neutral'
+      fill_in "Description", with: 'example'
+      fill_in "Hour", with: '12:32'
+      click_on 'Create Entry'
     end
 
-    it "renders" do
-      # expect(response).to render_template(:index)
+    context "submit a form succesfully" do
+      it "redirect to correct page" do
+        # expect(page).to redirect_to("entry_path")
+      end
+
+      it "render correct message" do
+        expect(page).to have_content("Entry was successfully created.")
+      end
+    end
+  end
+
+  describe '' do
+    context "error submitting a form" do
+
     end
   end
 end
