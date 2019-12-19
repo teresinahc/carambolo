40d2bd72d8f2a7031127e49e16cccb864b362334
Renata Marques <renata.marques@codeminer42.com>
2019-12-19 09:47:19 -0300

    fixing identation and specs message

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Home", type: :request do
   describe "GET /home" do
-    it "works! (now write some real specs)" do
+    it "returns the correct response code" do
       get root_path
       expect(response).to have_http_status(200)
     end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /users" do
-    it "works!" do
+    it "returns the correct response code" do
       get users_path
       expect(response).to have_http_status(200)
     end
