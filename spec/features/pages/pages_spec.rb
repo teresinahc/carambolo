e5609e824e508253aedc5b96adbfea0436a94d2b
Renata Marques <renata.marques@codeminer42.com>
2020-01-06 09:40:42 -0300

    fixing identation issues

--- spec/features/pages/pages_spec.rb
+++ spec/features/pages/pages_spec.rb
@@ -2,29 +2,28 @@ require "rails_helper"
 
 RSpec.describe 'home page', type: :feature do
   describe 'texts in home page' do
+    before { visit root_path }
 
-  	before { visit root_path }
-
-  	context 'match texts in home page' do
+    context 'match texts in home page' do
       it 'expect correct title' do
-	       expect(page).to have_content('Projeto Carambolo!')
+        expect(page).to have_content('Projeto Carambolo!')
       end
 
-  	  it 'expect correct description' do
-  	    expect(page).to have_content('Um projeto pela vida')
-  	  end
+      it 'expect correct description' do
+        expect(page).to have_content('Um projeto pela vida')
+      end
 
-  	  it 'expect correct section about' do
-  	    expect(page).to have_content('Sobre')
-  	  end
+      it 'expect correct section about' do
+        expect(page).to have_content('Sobre')
+      end
 
-  	  it 'expect correct section projects' do
-  	    expect(page).to have_content('Projetos')
-  	  end
+      it 'expect correct section projects' do
+        expect(page).to have_content('Projetos')
+      end
 
-  	  it 'expect correct article' do
-  	    expect(page).to have_content('Artigo sobre a técnica das três colunas')
-  	  end
-	  end
+      it 'expect correct article' do
+        expect(page).to have_content('Artigo sobre a técnica das três colunas')
+      end
+    end
   end
 end
