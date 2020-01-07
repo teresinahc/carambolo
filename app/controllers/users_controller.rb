64ab265e79ef0d568e861bc7e90a539dba35bf71
Renata Marques <renata.marques@codeminer42.com>
2020-01-07 10:35:21 -0300

    fixing user authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   def index
     @users = User.all
